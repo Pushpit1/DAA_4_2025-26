519dc166e5785d7cd9c4b4e793cbdf254a958844
Pushpit1 <81951634+Pushpit1@users.noreply.github.com>
2026-02-20 10:20:20 +0530

    code

--- session 16/climbing_stairs.cpp
+++ session 16/climbing_stairs.cpp
@@ -1,34 +1,19 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if (n < 3)
+            return n;
 
-int climbStairs(int n)
-{
-    if (n <= 2)
-        return n;
+        int prev = 1;      
+        int curr = 2;      
 
-    vector<int> ways(n + 1);
+        for (int i = 3; i <= n; i++) {
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
 
-    ways[1] = 1;
-    ways[2] = 2;
-
-    for (int i = 3; i <= n; i++)
-    {
-        ways[i] = ways[i - 1] + ways[i - 2];
+        return curr;
     }
-
-    return ways[n];
-}
-
-int main()
-{
-    int n;
-    cout << "Enter number of stairs: ";
-    cin >> n;
-
-    int result = climbStairs(n);
-
-    cout << "Number of distinct ways: " << result << endl;
-
-    return 0;
-}
+};
