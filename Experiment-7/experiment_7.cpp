3cbe8163abe9a0c9cd98ead2b068551679eaafe9
Pushpit1 <81951634+Pushpit1@users.noreply.github.com>
2026-03-30 22:00:59 +0530

    new

---
+++ Experiment-7/experiment_7.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    public:
+        int findCheapestPrice(int n, vector<vector<int>>& flights, int s, int d, int k) {
+            
+            vector<vector<pair<int,int>>> graph(n);
+            for(int i = 0; i < flights.size(); i++) {
+                int from = flights[i][0];
+                int to = flights[i][1];
+                int cost = flights[i][2];
+                graph[from].push_back({to, cost});
+            }
+    
+            vector<int> best(n, INT_MAX);
+            best[s] = 0;
+    
+            queue<vector<int>> q;
+            q.push({s, 0, 0});
+    
+            while(!q.empty()) {
+                vector<int> cur = q.front();
+                q.pop();
+    
+                int node = cur[0];
+                int costSoFar = cur[1];
+                int steps = cur[2];
+    
+                if(steps > k) continue;
+    
+                for(int i = 0; i < graph[node].size(); i++) {
+                    int nextNode = graph[node][i].first;
+                    int edgeCost = graph[node][i].second;
+    
+                    int newCost = costSoFar + edgeCost;
+    
+                    if(newCost < best[nextNode]) {
+                        best[nextNode] = newCost;
+    
+                        vector<int> temp;
+                        temp.push_back(nextNode);
+                        temp.push_back(newCost);
+                        temp.push_back(steps + 1);
+    
+                        q.push(temp);
+                    }
+                }
+            }
+    
+            return best[d] == INT_MAX ? -1 : best[d];
+        }
+    };
